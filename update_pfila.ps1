44942908151cc6ba8cd5466b68a6b700a329ec83
renatormc <32611729+renatormc@users.noreply.github.com>
2023-05-25 19:46:26 -0300

    Update update_pfila.ps1

--- update_pfila.ps1
+++ update_pfila.ps1
@@ -1,2 +1,4 @@
-$dest = $args[0]
-Write-Host $dest
+$destFolder = $args[0]
+$sourceFolder = "\\10.147.20.22\storage\renato\fotos"
+$robocopyArgs = "/mir /z /fft /r:3 /w:5"
+robocopy $sourceFolder $destinationFolder $robocopyArgs

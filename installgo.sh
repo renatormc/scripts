7f5d51c4646ec3fc5610967226eff3f80f4715f2
Renato Martins <32611729+renatormc@users.noreply.github.com>
2025-01-19 20:42:56 -0300

    Update installgo.sh

--- installgo.sh
+++ installgo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 src_folder="~/.renatormc/src"
 mkdir $src_folder -p
-cd $src_folder && git clone https://github.com/renatormc/$1 && cd $1 && ./install
+cd $src_folder && git clone https://github.com/renatormc/$1 && cd $1 && chmod +x ./install.sh && ./install.sh
